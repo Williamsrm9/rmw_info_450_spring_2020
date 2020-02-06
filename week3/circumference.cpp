094004c7a61d12c155622a06cdef3786884ddac3
Williamsrm9 <59450071+Williamsrm9@users.noreply.github.com>
2020-02-06 13:52:25 -0500

    Create circumference.cpp

---
+++ week3/circumference.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const float PI = 3.14159265358979323846;
+
+int main ()
+{
+    double calculateCircumference(double radius);
+    
+    cout << "Please enter the radius of the circle:";
+    
+    cin >> radius;
+    
+    
