01a848c3c23f452d854418a8c22c3b0cf3f0595d
Williamsrm9 <59450071+Williamsrm9@users.noreply.github.com>
2020-02-06 15:08:11 -0500

    Update circumference.cpp

--- week3/circumference.cpp
+++ week3/circumference.cpp
@@ -3,13 +3,27 @@
 using namespace std;
 
 const float PI = 3.14159265358979323846;
+double calculateCircumference(double radius);
 
 int main ()
 {
-    double calculateCircumference(double radius);
+
+    double radius;
     
-    cout << "Please enter the radius of the circle:";
+    cout << "Please enter the radius of the circle:" << endl;
     
     cin >> radius;
     
     
+    cout << "The circumference of the circle is: " << calculateCircumference << endl;
+    
+    return 0;
+    
+}
+
+double calculateCircumference(double radius)
+{
+    int i;
+    
+    i = (radius * 2 * PI);
+}
