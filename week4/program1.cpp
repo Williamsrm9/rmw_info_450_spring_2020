48c4fa481832242d39471c23d3795c49793d0d97
Williamsrm9 <59450071+Williamsrm9@users.noreply.github.com>
2020-02-13 18:23:36 -0500

    Update program1.cpp

--- week4/program1.cpp
+++ week4/program1.cpp
@@ -12,54 +12,27 @@ int main()
     double SqDifMean;
     double StdDev;
     
+    int t;
+    
     cout << "-----This program will calculate the mean and standard deviation for a set of 10 numbers. Please enter in 10 whole numbers-----" << endl;
     cout << "" << endl;
     
-    cout << "Enter the number for index 0:" << endl;
-    cin >> integer_array[0];
+    for(t =0; t<10; t++) {
     
-    cout << "Enter the number for index 1:" << endl;
-    cin >> integer_array[1];
+    cout << "Enter the number for index " << t << ": ";
+    cin >> integer_array[t];
     
-    cout << "Enter the number for index 2:" << endl;
-    cin >> integer_array[2];
-    
-    cout << "Enter the number for index 3:" << endl;
-    cin >> integer_array[3];
-    
-    cout << "Enter the number for index 4:" << endl;
-    cin >> integer_array[4];
-    
-    cout << "Enter the number for index 5:" << endl;
-    cin >> integer_array[5];
-    
-    cout << "Enter the number for index 6:" << endl;
-    cin >> integer_array[6];
-    
-    cout << "Enter the number for index 7:" << endl;
-    cin >> integer_array[7];
-    
-    cout << "Enter the number for index 8:" << endl;
-    cin >> integer_array[8];
-    
-    cout << "Enter the number for index 9:" << endl;
-    cin >> integer_array[9];
-    
+    }
     
     sum = integer_array[0] + integer_array[1] + integer_array[2] + integer_array[3] + integer_array[4] + integer_array[5] + integer_array[6] + integer_array[7] + integer_array[8] + integer_array[9];
     
     mean = sum / 10;
     
-    sqdif_array[0] = pow((integer_array[0] - mean), 2);
-    sqdif_array[1] = pow((integer_array[1] - mean), 2);
-    sqdif_array[2] = pow((integer_array[2] - mean), 2);
-    sqdif_array[3] = pow((integer_array[3] - mean), 2);
-    sqdif_array[4] = pow((integer_array[4] - mean), 2);
-    sqdif_array[5] = pow((integer_array[5] - mean), 2);
-    sqdif_array[6] = pow((integer_array[6] - mean), 2);
-    sqdif_array[7] = pow((integer_array[7] - mean), 2);
-    sqdif_array[8] = pow((integer_array[8] - mean), 2);
-    sqdif_array[9] = pow((integer_array[9] - mean), 2);
+    for(t =0; t<10; t++) {
+    
+    sqdif_array[t] = pow((integer_array[t] - mean), 2);
+    
+    }
     
     SqDifSum = sqdif_array[0] + sqdif_array[1] + sqdif_array[2] + sqdif_array[3] + sqdif_array[4] + sqdif_array[5] + sqdif_array[6] + sqdif_array[7] + sqdif_array[8] + sqdif_array[9];
     
@@ -69,3 +42,5 @@ int main()
     
     cout << "" << endl;
     cout << "RESULTS: Mean = " << mean << " Standard Deviation = " << StdDev << endl;
+    
+}
